b4fb87c190f80764702289f0cb56e79511b01105
joaogabriellyra <jgabriellyra@hotmail.com>
2023-10-14 09:25:55 -0300

    corrigindo erros

--- exercicio-um/exerciseOne.ts
+++ exercicio-um/exerciseOne.ts
@@ -3,15 +3,15 @@
 // b) Dar um exemplo de uso com uma palavra recebida via input no formulário.
 const input = document.querySelector("#vowelCounterInput") as HTMLInputElement;
 const button = document.querySelector("#button") as HTMLButtonElement;
-const result = document.querySelector("#result") as HTMLTitleElement
+const result = document.querySelector("#result") as HTMLTitleElement;
 
 const checkingVowel = (vowel: string):boolean => {
-    const vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
+    const vowels: string[] = ["a", "e", "i", "o", "u"];
     return vowels.includes(vowel);
 };
 
 const vowelCounter = (vowelsWord: string):string => {
-    const word = String(vowelsWord)
+    const word = String(vowelsWord);
     const vowels: string[] = word.toLowerCase().split("").filter(checkingVowel);
     return String(vowels.length);
 };
